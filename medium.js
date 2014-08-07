3fe3fd92b5d60e27411d636b1edbc1fb9ae58f66
Brian Link <cpsubrian@gmail.com>
2014-08-07 12:09:11 -0700

    Fixes removeChild() on placeholder not in the DOM
    If the Medium instance already has content when created, the placeholder will not be in the DOM and so cannot be removed. Causes the following error:
    
    ```
    TypeError: Cannot read property 'removeChild' of null (medium.js:599)
    ```

--- medium.js
+++ medium.js
@@ -594,7 +594,7 @@ var Medium = (function (w, d) {
                 settings = this.settings,
                 placeholder = this.placeholder || null;
 
-            if (placeholder !== null) {
+            if (placeholder !== null && placeholder.setup) {
                 //remove placeholder
                 placeholder.parentNode.removeChild(placeholder);
             }
